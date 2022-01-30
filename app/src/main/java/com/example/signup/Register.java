f34aedf269753c2b2c013745118ecf1e07808e70
91881 <sajwanrah020@gmail.com>
2022-01-30 15:11:50 +0530

    Commit

--- app/src/main/java/com/example/signup/Register.java
+++ app/src/main/java/com/example/signup/Register.java
@@ -2,11 +2,20 @@ package com.example.signup;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,10 +24,73 @@ import android.view.ViewGroup;
  */
 public class Register extends Fragment {
 
+    EditText name, username, email, pass, compass, cc, phone;
+    Button register;
+    Boolean isDataValid = false;
+    FirebaseAuth fAuth;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View v = inflater.inflate(R.layout.fragment_register, container, false);
+        name = v.findViewById(R.id.username);
+        username = v.findViewById(R.id.userusername);
+        email = v.findViewById(R.id.useremail);
+        pass = v.findViewById(R.id.password);
+        compass = v.findViewById(R.id.compass);
+        cc = v.findViewById(R.id.CC);
+        phone = v.findViewById(R.id.phonenum);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        //validating the data
+
+        validateData(name);
+        validateData(username);
+        validateData(email);
+        validateData(pass);
+        validateData(cc);
+        validateData(compass);
+        validateData(phone);
+
+        if(!pass.getText().toString().equals(compass.getText().toString())){
+
+            isDataValid = false;
+            compass.setError("Password Do Not Match");
+        }else {
+
+            isDataValid = true;
+        }
+
+        if (isDataValid){
+            //proceed with the registration
+            fAuth.createUserWithEmailAndPassword(email.getText().toString(), compass.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Toast.makeText(getActivity(), "User Account is Created", Toast.LENGTH_SHORT).show();
+                    //send User to verify Phone number
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getActivity(), "Error!", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+
+        return v;
+    }
+
+    public void validateData(EditText field){
+
+        if (field.getText().toString().isEmpty()){
+            isDataValid = false;
+            field.setError("Required field");
+        }else {
+            isDataValid = true;
+        }
+
     }
 }
